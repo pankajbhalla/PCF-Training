d82f0596e87fa92d328e2ae183e2c2137ac3ad32
Pankaj Bhalla <41019318+pankajbhalla@users.noreply.github.com>
2020-02-13 10:50:29 +0530

    Add files via upload
    These Projects can be used for a hands-on regarding PCF deployments

---
+++ todo-web-application-h2/target/classes/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, 'Learn Spring Boot', false, sysdate(), 'pcftraining');
+insert into TODO
+values(10002, 'Learn Angular JS', false, sysdate(), 'pcftraining');
+insert into TODO
+values(10003, 'Learn to Dance', false, sysdate(), 'pcftraining');
